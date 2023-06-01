984846b04a5d802ee03e59a23e66db95c6416d32
hamza <hamza>
2023-06-01 09:48:28 +0000

    Using the Result Type

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,23 +1,21 @@
 use assert_cmd::Command;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicates::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
-}
-
-#[test]
-fn hello1() {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
-    let expected = std::fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let expected = std::fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
